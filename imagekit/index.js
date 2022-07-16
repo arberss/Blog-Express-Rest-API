2ccbd960804d1debd779187c241dc5e0c05d1562
arberss <arberssalihuu@gmail.com>
2022-07-16 14:09:37 +0200

    env keys

--- imagekit/index.js
+++ imagekit/index.js
@@ -1,7 +1,8 @@
 const ImageKit = require('imagekit');
+require('dotenv').config();
 
 exports.imagekit = new ImageKit({
-  publicKey: 'public_fJZt0HphVK+D6QxBkXBO7YUL/yE=',
-  privateKey: 'private_v4nlCXg6IkMlvMFZun3HkVZyCGk=',
-  urlEndpoint: 'https://ik.imagekit.io/blogexpress',
+  publicKey: `${process.env.IMAGEKIT_PUBLIC_KEY}`,
+  privateKey: `${process.env.IMAGEKIT_PRIVATE_KEY}`,
+  urlEndpoint: `${process.env.IMAGEKIT_URL}`,
 });
